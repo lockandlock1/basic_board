26bbc478a2924add5bd4067cbb0b6fea5ed3f8e7
redkyu <hongstar2311@gmail.com>
2023-07-20 23:57:47 +0900

    thymeleaf & 테스트 코드 추가

---
+++ src/main/java/com/hong/basic_board/controller/ArticleController.java
@@ -0,0 +1,13 @@
+package com.hong.basic_board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/articles")
+public class ArticleController {
+
+
+}
